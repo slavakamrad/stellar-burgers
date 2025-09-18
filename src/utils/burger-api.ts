5d89c76552b01243461b84e367f8007e8888373b
slavakamrad <slavakamrad@yandex.ru>
2025-09-18 23:40:59 +0300

    git fix api connect

--- src/utils/burger-api.ts
+++ src/utils/burger-api.ts
@@ -1,7 +1,9 @@
 import { setCookie, getCookie } from './cookie';
 import { TIngredient, TOrder, TOrdersData, TUser } from './types';
 
-const URL = process.env.BURGER_API_URL;
+const URL = process.env.REACT_APP_BURGER_API_URL;
+
+console.log(URL);
 
 const checkResponse = <T>(res: Response): Promise<T> =>
   res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
