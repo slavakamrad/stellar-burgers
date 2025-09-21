b2d6ecddc0f99dc560be5b53e170b39e6577ff98
slavakamrad <slavakamrad@yandex.ru>
2025-09-21 13:55:33 +0300

    update app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -23,6 +23,7 @@ import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 import { FC, useEffect } from 'react';
 import { useDispatch } from '../../services/store';
 import { fetchIngredients } from '../../services/slices/ingridientsSlice';
+import { checkUserAuth } from '../../services/slices/userSlice';
 
 const App: FC = () => {
   const dispatch = useDispatch();
@@ -32,6 +33,7 @@ const App: FC = () => {
 
   useEffect(() => {
     dispatch(fetchIngredients());
+    dispatch(checkUserAuth());
   }, [dispatch]);
 
   const handleModalClose = () => {
