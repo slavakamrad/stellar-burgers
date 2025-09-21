9212d17f36a302204e79d5dccd55cc6f8f2a9e61
slavakamrad <slavakamrad@yandex.ru>
2025-09-21 21:49:57 +0300

    update profile and register

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,13 +1,24 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { useDispatch } from '../../services/store';
+import { useNavigate } from 'react-router-dom';
+import { registerUser } from '../../services/slices/userSlice';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(registerUser({ email, password, name: userName }))
+      .unwrap()
+      .then(() => {
+        navigate('/');
+      })
+      .catch(() => {});
   };
 
   return (
