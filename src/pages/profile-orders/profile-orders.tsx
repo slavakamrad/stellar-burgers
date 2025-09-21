df3260f385451a8fca0857dcccd1458c769fd1e3
slavakamrad <slavakamrad@yandex.ru>
2025-09-21 22:35:28 +0300

    update profile-orders

--- src/pages/profile-orders/profile-orders.tsx
+++ src/pages/profile-orders/profile-orders.tsx
@@ -1,10 +1,25 @@
 import { ProfileOrdersUI } from '@ui-pages';
 import { TOrder } from '@utils-types';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
+import { useSelector, useDispatch } from '../../services/store';
+import { fetchFeed } from '../../services/slices/feedSlice';
+import { Preloader } from '../../components/ui/preloader/preloader';
 
 export const ProfileOrders: FC = () => {
-  /** TODO: взять переменную из стора */
-  const orders: TOrder[] = [];
+  const dispatch = useDispatch();
+  const orders: TOrder[] = useSelector((state) => state.feed.orders);
+  const user = useSelector((state) => state.user.user);
+  const loading = useSelector((state) => state.feed.loading);
 
-  return <ProfileOrdersUI orders={orders} />;
+  useEffect(() => {
+    dispatch(fetchFeed());
+  }, [dispatch]);
+
+  const userOrders = orders.filter((order) => order.name === user?.name);
+
+  if (loading) {
+    return <Preloader />;
+  }
+
+  return <ProfileOrdersUI orders={userOrders} />;
 };
