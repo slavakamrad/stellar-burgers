6c547cd51f8ba35c1baca4d4e331ccde9c202522
slavakamrad <slavakamrad@yandex.ru>
2025-10-05 22:55:36 +0300

    add constructor.cy.tsx

---
+++ cypress/e2e/constructor.cy.tsx
@@ -0,0 +1,13 @@
+describe('Burger constructor', () => {
+  beforeEach(() => {});
+
+  describe('Add ingridients in constructor', () => {});
+
+  describe('Modal functionality', () => {
+    it('Modal open-close', () => {});
+  });
+
+  describe('Order create', () => {
+    it('Add order and check number', () => {});
+  });
+});
