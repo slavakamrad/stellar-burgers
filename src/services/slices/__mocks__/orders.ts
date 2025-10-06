8c8072bbfa31847058423497b19810771bf8d89e
slavakamrad <slavakamrad@yandex.ru>
2025-10-06 23:45:17 +0300

    add test for orderSlice

---
+++ src/services/slices/__mocks__/orders.ts
@@ -0,0 +1,34 @@
+import { TOrder } from "@utils-types";
+
+export const mockOrders: TOrder[] = [
+    {
+      _id: '1',
+      ingredients: [],
+      status: 'done',
+      name: 'Test Order',
+      createdAt: '2023-01-01T00:00:00.000Z',
+      updatedAt: '2023-01-01T00:00:00.000Z',
+      number: 12345
+    },
+        {
+      _id: '2',
+      ingredients: [],
+      status: 'done',
+      name: 'Test Order2',
+      createdAt: '2023-01-01T00:00:00.000Z',
+      updatedAt: '2023-01-01T00:00:00.000Z',
+      number: 12346
+    }
+  ];
+
+export const mockOrder: TOrder = 
+    {
+      _id: '1',
+      ingredients: [],
+      status: 'done',
+      name: 'Test Order',
+      createdAt: '2023-01-01T00:00:00.000Z',
+      updatedAt: '2023-01-01T00:00:00.000Z',
+      number: 12345
+    }
+  
