c7970cd1cba6ea12c59b746d8140e7c516af928e
slavakamrad <slavakamrad@yandex.ru>
2025-10-10 22:26:14 +0300

    update cy config port

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost:4000'
+    baseUrl: 'http://localhost:8000'
   }
 });
